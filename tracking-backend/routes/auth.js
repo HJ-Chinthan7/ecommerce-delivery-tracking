90c06b08748abde6c24b95f36d60e20caafa0f8e
HJ-Chinthan7 <chinthanhj107@gmail.com>
2025-09-11 13:57:44 +0530

    validation added

--- tracking-backend/routes/auth.js
+++ tracking-backend/routes/auth.js
@@ -3,11 +3,19 @@ const jwt = require('jsonwebtoken');
 const Driver = require('../models/Driver');
 const Bus = require('../models/Bus');
 const driverController=require("../controllers/driverController");
+const {body}=require('express-validator');
 const router = express.Router();
 
-router.post('/busLogin',driverController.driverLogin);
+router.post('/busLogin',[body('email').isEmail().withMessage("Invalid email"),
+body('password').isLength({min:6}).withMessage("Password must be at least 6 characters"),
+],driverController.driverLogin);
 
 
-router.post('/registerDriver',driverController.driverRegister);
+router.post('/registerDriver',[body("email").isEmail().withMessage("Invalid Email"),
+body("password").isLength({min:6}).withMessage("Password must be at least 6 characters"),
+body("name").notEmpty().withMessage("Name is required"),
+body("driverId").notEmpty().withMessage("Driver ID is required"),
+body("busId").notEmpty().withMessage("Bus ID is required"),
+],driverController.driverRegister);
 
 module.exports = router;
