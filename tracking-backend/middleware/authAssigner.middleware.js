87ee096bee26ce3bd977102ef78b8fdc2ff9aed2
HJ-Chinthan7 <chinthanhj107@gmail.com>
2025-11-16 14:34:09 +0530

    consoel.log removed

--- tracking-backend/middleware/authAssigner.middleware.js
+++ tracking-backend/middleware/authAssigner.middleware.js
@@ -3,19 +3,16 @@ const jwt = require("jsonwebtoken");
 const Assigner = require("../models/Assigner");
 const authAssigner = async (req, res, next) => {
   let token = req.cookies?.token;
-  console.log("before",token);
    if(!token) {
     const authHeader = req.headers["authorization"];
      token = authHeader && authHeader.split(" ")[1];
     }
   if (!token) return res.status(401).json({ message: "Not authorized" });
-console.log("afetr token")
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const assigner = await Assigner.findById(decoded.userId).select("-password");
     if (!assigner) return res.status(401).json({ message: "Assigner not found" });
     req.assigner = assigner;
-    console.log("here after ")
     next();
   } catch (err) {
     console.error(err);
